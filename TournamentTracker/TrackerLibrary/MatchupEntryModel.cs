f57e8c67c1ffe9ac079826ac964b95349bb7351a
GLor1 <50379998+GLor1@users.noreply.github.com>
2019-12-06 11:15:12 -0600

    Add files via upload

---
+++ TournamentTracker/TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary
+{
+    public class MatchupEntryModel
+    {
+        /// <summary>
+        /// Represents one time in the match up.
+        /// </summary>
+        public TeamModel TeamCompeting { get; set; }
+
+        /// <summary>
+        /// Represents the score for the team
+        /// </summary>
+        public double Score { get; set; }
+
+        /// <summary>
+        /// Represents the matchup that this team came from.
+        /// </summary>
+        public MatchupModel ParentMatchup { get; set; }
+
+    }
+}
---
+++ TournamentTracker/TrackerLibrary/TournamentModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary
+{
+    public class TournamentModel
+    {
+        public string TournamentName { get; set; }
+        public decimal EntryFee { get; set; }
+        public List<TeamModels> EnteredTeams { get; set; } = new List<TeamModels>();
+        public List<PrizeModel> Prizes { get; set; } = new List<PrizeModel>();
+        public List<List<MatchUpModel>> Rounds { get; set; } new List<List<MatchupModel>>();
+    }
+}
